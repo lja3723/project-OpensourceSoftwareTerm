6219fb079fdfeee877d071eb801697e87166de36
lja3723 <lja3723@gmail.com>
2024-12-03 18:11:37 +0900

    feat: 사용가능한 sources 리스트 추가

--- news_notifier/news_collector.py
+++ news_notifier/news_collector.py
@@ -2,6 +2,22 @@ from newsapi import NewsApiClient
 from .utils import *
 
 newsapi = NewsApiClient(api_key='8af726a068d74a5588e34477b577a76d')
+newsapi_en_sources = [
+    "abc-news", "abc-news-au", "al-jazeera-english", "ars-technica", "associated-press",
+    "australian-financial-review", "axios", "bbc-news", "bbc-sport", "bleacher-report",
+    "bloomberg", "breitbart-news", "business-insider", "buzzfeed", "cbc-news", "cbs-news",
+    "cnn", "crypto-coins-news", "engadget", "entertainment-weekly", "espn", "espn-cric-info",
+    "financial-post", "football-italia", "fortune", "four-four-two", "fox-news", "fox-sports",
+    "google-news", "google-news-au", "google-news-ca", "google-news-in", "google-news-uk",
+    "hacker-news", "ign", "independent", "mashable", "medical-news-today", "msnbc", "mtv-news",
+    "mtv-news-uk", "national-geographic", "national-review", "nbc-news", "news24", "new-scientist",
+    "news-com-au", "newsweek", "new-york-magazine", "next-big-future", "nfl-news", "nhl-news",
+    "politico", "polygon", "recode", "reddit-r-all", "reuters", "rte", "talksport", "techcrunch",
+    "techradar", "the-american-conservative", "the-globe-and-mail", "the-hill", "the-hindu",
+    "the-huffington-post", "the-irish-times", "the-jerusalem-post", "the-lad-bible", "the-next-web",
+    "the-sport-bible", "the-times-of-india", "the-verge", "the-wall-street-journal", "the-washington-post",
+    "the-washington-times", "time", "usa-today", "vice-news", "wired"
+]
 
 def fun():
     # Init
