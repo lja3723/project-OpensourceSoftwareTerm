cf5e1a6118e25ba30b87fa69a3999168406dbecc
lja3723 <lja3723@gmail.com>
2024-12-14 17:54:35 +0900

    chore: entry 정리

--- entry.py
+++ entry.py
@@ -1,14 +1,7 @@
-from news_notifier import news_collector as nc
-from news_notifier.kakao_api import KakaoApi
-import time
 
 
 def main():
-    kakao_api = KakaoApi()
-    # kakao_api.login()
-    # kakao_api.refresh_token()
-    kakao_api.send_message()
-    # kakao_api.logout()
+    pass
 
 
 if __name__ == "__main__":
